7567515e2eb310f6fbfa1b5caaa0c8fba42f95ae
Nick Socha <38092580+nicksocha@users.noreply.github.com>
2019-09-16 01:10:11 -0500

    Added Typeform, Updated Logo
    Added href to index.html, a selector in style.css, and Typeform in app.js, Updated Logo in README.

--- js/app.js
+++ js/app.js
@@ -46,3 +46,24 @@ window.onkeydown = event => {
 // Automatically update year in footer
 const yyyy = new Date().getFullYear();
 document.getElementById('year').innerHTML = `SFHsounds #${yyyy}`;
+
+// Typeform
+(function() {
+  let qs;
+  let js;
+  let q;
+  let s;
+  const d = document;
+  const gi = d.getElementById;
+  const ce = d.createElement;
+  const gt = d.getElementsByTagName;
+  const id = 'typef_orm_share';
+  const b = 'https://embed.typeform.com/';
+  if (!gi.call(d, id)) {
+    js = ce.call(d, 'script');
+    js.id = id;
+    js.src = `${b}embed.js`;
+    q = gt.call(d, 'script')[0];
+    q.parentNode.insertBefore(js, q);
+  }
+})();
